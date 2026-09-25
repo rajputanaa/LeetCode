272aa1543adc1492e0e4be4c7782e6dfddc52e9b
Aditya Singh <161753293+rajputanaa@users.noreply.github.com>
2026-09-25 22:54:50 +0530

    Time: 509 ms (63.12%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,5 +1,13 @@
-SELECT w1.id
-FROM weather w1
-JOIN weather w2
-ON w1.recordDate = DATE_ADD(w2.recordDate , INTERVAL 1 DAY) 
-WHERE w2.temperature  < w1.temperature ;
+-- SELECT w1.id
+-- FROM weather w1
+-- JOIN weather w2
+-- ON w1.recordDate = DATE_ADD(w2.recordDate , INTERVAL 1 DAY) 
+-- WHERE w2.temperature  < w1.temperature ;
+
+
+SELECT t.id
+FROM weather y
+CROSS JOIN weather t
+
+WHERE DATEDIFF(t.recordDate , y.recordDate) = 1 
+AND t.temperature > y.temperature;
