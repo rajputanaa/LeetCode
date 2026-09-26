a90dffb86a47942ed8eecd2b0622d86a4a2bea64
Aditya Singh <161753293+rajputanaa@users.noreply.github.com>
2026-09-26 17:35:42 +0530

    Time: 3 ms (100.00%) | Memory: 175.5 MB (58.02%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,29 +1,58 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
 
-    int n = nums.length;
-    int p1 =0;
-    int p2 =0;
+        /********* First approach ***********/
 
-    int pos[] = new int[n/2];
-    int neg[] = new int[n/2];
+//     int n = nums.length;
+//     int p1 =0;
+//     int p2 =0;
 
-    for(int i=0; i<n; i++){
-        if(nums[i]>=0){
-            pos[p1] = nums[i];
-            p1++;
-        }
-        else{
-            neg[p2] = nums[i];
-            p2++;
-        }
+//     int pos[] = new int[n/2];
+//     int neg[] = new int[n/2];
+
+//     for(int i=0; i<n; i++){
+//         if(nums[i]>=0){
+//             pos[p1] = nums[i];
+//             p1++;
+//         }
+//         else{
+//             neg[p2] = nums[i];
+//             p2++;
+//         }
+//     }
+
+//     for(int i=0; i<n/2; i++){
+//         nums[i*2] = pos[i];
+//         nums[i*2+1] = neg[i];
+//     }
+
+//    return nums;
+
+
+/**************** second approach *****************/
+
+    int pos =0;
+    int neg =1;
+    int ans[] = new int[nums.length];
+
+    for(int i=0; i<nums.length; i++){
+    if(nums[i]>=0){
+        ans[pos] = nums[i];
+        pos+=2;
     }
-
-    for(int i=0; i<n/2; i++){
-        nums[i*2] = pos[i];
-        nums[i*2+1] = neg[i];
+    else{
+        ans[neg] = nums[i];
+        neg+=2;
     }
+    }
+    return ans;
 
-   return nums;
+
+
+
     }
 }
+
+
+
+
